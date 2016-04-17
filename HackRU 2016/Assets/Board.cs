4fe2d6df25b93b9a0322c9ea669e809ce5f277a3
Tyler C. Roach <troach06@yahoo.com>
2016-04-17 03:30:14 -0400

    Version 3

---
+++ HackRU 2016/Assets/Board.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Board : MonoBehaviour
+    {
+    Vector3 initposition = new Vector3(-6.5f, -0.1f, 3);
+    Quaternion dartrot = Quaternion.Euler(0, 90, 0);
+    // Use this for initialization
+    void Start()
+        {
+
+        }
+
+    // Update is called once per frame
+    void Update()
+        {
+
+        }
+    void OnTriggerEnter(Collider other) // C#, type first, name in second
+        {
+        if (other.gameObject.tag == "Board")
+            {
+            Debug.Log("Grab");
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+            StartCoroutine(Reset());
+            }
+        if (other.gameObject.tag == "Wall")
+            {
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+            StartCoroutine(Reset());
+            }
+        }
+    public IEnumerator Reset()
+        {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("Darts");
+        }
+    }
---
+++ HackRU 2016/Assets/GrabRight.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using System.Collections.Generic;
+using Leap.Unity;
+
+public class GrabRight : MonoBehaviour
+{
+    Quaternion dartrot = Quaternion.Euler(0, 90, 0);
+
+    LeapProvider provider;
+    public GameObject dart;
+    public Transform lefthand;
+    public Transform righthand;
+    public AudioSource woosh;
+   /// Vector3 modelpos = new Vector3(0, 0.6f, -9.5f);
+  //  Quaternion modelrot = Quaternion.Euler(0, 0, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        //woosh.enabled = false;
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+        dart.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        dart = GameObject.FindWithTag("Dart");
+        Frame frame = provider.CurrentFrame; // controller is a Controller object
+        List<Hand> hands = frame.Hands;
+        for (int h = 0; h < hands.Count; h++)
+        {
+            Hand hand = hands[h];
+            if (dart.transform.parent == righthand && hand.IsRight && hand.GrabStrength == 0)
+            {
+                dart.transform.parent = null;
+                dart.GetComponent<Rigidbody>().isKinematic = false;
+                dart.GetComponent<Rigidbody>().AddForce(-6f, 0f, 0f, ForceMode.Impulse);
+                dart.transform.rotation = (dartrot);
+               // model.transform.position = modelpos;
+               // model.transform.rotation = modelrot;
+               // woosh.enabled = false;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other) // C#, type first, name in second
+    {
+        Frame frame = provider.CurrentFrame; // controller is a Controller object
+        List<Hand> hands = frame.Hands;
+        for (int h = 0; h < hands.Count; h++)
+        {
+            Hand hand = hands[h];
+            if (other.gameObject.tag == "Dart")
+            {
+                if (hand.IsRight && hand.GrabStrength > 0.1)
+                {
+                    Debug.Log("Grab");
+                    dart.GetComponent<Rigidbody>().isKinematic = true;
+                    dart.transform.parent = righthand;
+                    //woosh.enabled = true;
+                }
+            }
+        }
+    }
+}
+
+
